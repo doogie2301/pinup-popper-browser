d207d31b87b7e9ebf2a631413e5d20907f5d8852
doogie2301 <doogie2301@users.noreply.github.com>
2020-05-27 21:28:29 -0400

    formatting and logging updates

--- app.js
+++ app.js
@@ -1,81 +1,106 @@
-'use strict';
-var debug = require('debug');
-var express = require('express');
-var favicon = require('serve-favicon');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+"use strict";
+var debug = require("debug");
+var express = require("express");
+var favicon = require("serve-favicon");
+var path = require("path");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 var settings = require("app-settings");
-var routeIndex = require('./routes/index');
-var routeGame = require('./routes/game');
+var routeIndex = require("./routes/index");
+var routeGame = require("./routes/game");
 
 var app = express();
-app.disable('x-powered-by');
+
+const httpLogger = debug("app:http");
+app.use(
+  logger(settings.httpServer.logFormat, {
+    stream: {
+      write: (msg) => {
+        httpLogger(msg.trimEnd());
+      },
+    },
+    skip: (req, res) => {
+      return settings.httpServer.logLevel == "error"
+        ? res.statusCode < 400
+        : false;
+    },
+  })
+);
+
+app.disable("x-powered-by");
 
 var games = [];
 var gameIds = new Map();
 var globalSettings = new Object();
 var emulators = new Map();
 
-const log = debug('popperRemote');
-
 // open the database
-const db = require('better-sqlite3')(settings.pupServer.db.path, { fileMustExist: true, verbose: log });
-
-// get global settings
-const globalRow = db.prepare("SELECT GlobalMediaDir, ThumbRotate, AttractModeinterval FROM GlobalSettings").get();
-if (globalRow) {
-    globalSettings.defaultMediaDir = globalRow.GlobalMediaDir;
-    globalSettings.thumbRotation = globalRow.ThumbRotate;
-    globalSettings.currentGameRefreshTimer = (globalRow.AttractModeinterval || 0) * 1000;
-} else {
-    console.error("GlobalSettings not found");
-}
-
-
-// get emulators
-const emuRows = db.prepare("SELECT EMUID, EmuDisplay, DirMedia FROM Emulators WHERE Visible").all();
-emuRows.forEach((row) => {
-    emulators.set(row.EMUID,
-        {
-            id: row.EMUID,
-            name: row.EmuDisplay,
-            dirMedia: row.DirMedia || globalSettings.defaultMediaDir
-        });
+const db = require("better-sqlite3")(settings.pupServer.db.path, {
+  fileMustExist: true,
+  verbose: debug("app:sql"),
 });
 
-let sql = 'select g.gameid, g.emuID, gamename, gamedisplay, gametype, gameyear, numplayers, manufact, LastPlayed, NumberPlays, TimePlayedSecs, '
-    + 'category, gametheme, isFav '
-    + 'from games g join emulators e on g.emuid = e.emuid '
-    + 'left join (SELECT GameID, 1 as isFav FROM Playlistdetails WHERE isFav > 0 GROUP BY GameID) f on g.gameid = f.gameid '
-    + 'left join gamesstats s on g.gameid = s.gameid '
-    + 'where g.visible and e.visible '
-    + (settings.pupServer.db.filter ? ' and ' + settings.pupServer.db.filter : '')
-    + 'order by gamedisplay';
+// get global settings
+const globalRow = db
+  .prepare(
+    "SELECT GlobalMediaDir, ThumbRotate, AttractModeinterval FROM GlobalSettings"
+  )
+  .get();
+if (globalRow) {
+  globalSettings.defaultMediaDir = globalRow.GlobalMediaDir;
+  globalSettings.thumbRotation = globalRow.ThumbRotate;
+  globalSettings.currentGameRefreshTimer =
+    (globalRow.AttractModeinterval || 0) * 1000;
+} else {
+  console.error("GlobalSettings not found");
+}
+
+// get emulators
+const emuRows = db
+  .prepare("SELECT EMUID, EmuDisplay, DirMedia FROM Emulators WHERE Visible")
+  .all();
+emuRows.forEach((row) => {
+  emulators.set(row.EMUID, {
+    id: row.EMUID,
+    name: row.EmuDisplay,
+    dirMedia: row.DirMedia || globalSettings.defaultMediaDir,
+  });
+});
+
+let sql =
+  "select g.gameid, g.emuID, gamename, gamedisplay, gametype, gameyear, numplayers, manufact, LastPlayed, NumberPlays, TimePlayedSecs, " +
+  "category, gametheme, isFav " +
+  "from games g join emulators e on g.emuid = e.emuid " +
+  "left join (SELECT GameID, 1 as isFav FROM Playlistdetails WHERE isFav > 0 GROUP BY GameID) f on g.gameid = f.gameid " +
+  "left join gamesstats s on g.gameid = s.gameid " +
+  "where g.visible and e.visible " +
+  (settings.pupServer.db.filter ? " and " + settings.pupServer.db.filter : "") +
+  "order by gamedisplay";
 
 const rows = db.prepare(sql).all();
-    var i = 0;
+var i = 0;
 rows.forEach((row) => {
-    games.push(
-        {
-            id: row.GameID,
-            name: row.GameName,
-            display: row.GameDisplay,
-            type: row.GameType,
-            category: row.Category,
-            theme: row.GameTheme,
-            year: row.GameYear,
-            numPlayers: row.NumPlayers,
-            manufacturer: row.Manufact,
-            emulator: emulators.get(row.EMUID),
-            lastPlayed: row.LastPlayed,
-            numPlays: row.NumberPlays || 0,
-            timePlayed: row.TimePlayedSecs || 0,
-            decade: row.GameYear ? parseInt(row.GameYear) - (parseInt(row.GameYear) % 10) : '',
-            favorite: row.isFav
-        });
-    gameIds.set(row.GameID, i);
-    i++;
+  games.push({
+    id: row.GameID,
+    name: row.GameName,
+    display: row.GameDisplay,
+    type: row.GameType,
+    category: row.Category,
+    theme: row.GameTheme,
+    year: row.GameYear,
+    numPlayers: row.NumPlayers,
+    manufacturer: row.Manufact,
+    emulator: emulators.get(row.EMUID),
+    lastPlayed: row.LastPlayed,
+    numPlays: row.NumberPlays || 0,
+    timePlayed: row.TimePlayedSecs || 0,
+    decade: row.GameYear
+      ? parseInt(row.GameYear) - (parseInt(row.GameYear) % 10)
+      : "",
+    favorite: row.isFav,
+  });
+  gameIds.set(row.GameID, i);
+  i++;
 });
 
 // close the database connection
@@ -86,70 +111,75 @@ app.locals.gameIds = gameIds;
 app.locals.globalSettings = globalSettings;
 
 app.locals.getWheelSrc = function (game) {
-    return '/media' + game.emulator.id + '/Wheel/' + (settings.media.useThumbs ? 'pthumbs/' + game.name + '_thumb' : game.name) + '.png';
-};   
-
+  let name = game.name;
+  if (settings.media.useThumbs) {
+    name = "pthumbs/" + name + "_thumb";
+  }
+  return "/media_" + game.emulator.id + "/Wheel/" + name + ".png";
+};
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let cacheOptions = {
-    maxAge: settings.media.cacheInMinutes * 60000,
-    immutable: true
-}
-app.use(express.static(path.join(__dirname, 'public'), cacheOptions));
+  maxAge: settings.media.cacheInMinutes * 60000,
+  immutable: true,
+};
+app.use(express.static(path.join(__dirname, "public"), cacheOptions));
 
 emulators.forEach(function (value, key) {
-    let dir = '/media' + value.id;
-    let path = value.dirMedia;
-    app.use(dir, express.static(path, cacheOptions));
-    log("Set media path '" + dir + "' to '" + path + "' for emulator '" + value.name + "'");
+  let dir = "/media_" + value.id;
+  let path = value.dirMedia;
+  app.use(dir, express.static(path, cacheOptions));
+  debug("app:media")(
+    "Set media path '%s' to '%s' for emulator '%s'",
+    dir,
+    path,
+    value.name
+  );
 });
 
-app.use('/games', routeGame);
-app.use('/', routeIndex);
-
+app.use("/games", routeGame);
+app.use("/", routeIndex);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render("error", {
+      message: err.message,
+      error: err,
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: {},
+  });
 });
 
+app.set("port", process.env.PORT || settings.httpServer.port);
 
-app.set('port', process.env.PORT || settings.httpServer.port);
-
-var server = app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + server.address().port);
+var server = app.listen(app.get("port"), function () {
+  console.log("Express server listening on port %i", server.address().port);
+  httpLogger("test2");
 });
--- routes/game.js
+++ routes/game.js
@@ -1,156 +1,187 @@
-var express = require('express');
+var express = require("express");
 var settings = require("app-settings");
 var router = express.Router();
 const axios = require("axios");
-var glob = require("glob")
-const path = require('path');
+var glob = require("glob");
+const path = require("path");
+var debug = require("debug");
 
 var getCurItemUrl = settings.pupServer.url + "/function/getcuritem";
 var launchUrl = settings.pupServer.url + "/function/launchgame/";
 var exitUrl = settings.pupServer.url + "/pupkey/15";
 
-router.get('/:gameId/info', function (req, res) {
-    let game = getGame(req.params["gameId"], req);
-    glob(getAbsoluteMediaPath(game) + "/GameInfo/" + game.name + "*.{png,jpg}", function (err, files) {
-        let result = [];
-        for (file of files) {
-            result.push(getRelativeMediaPath(game) + "/GameInfo/" + path.basename(file));
-        }
-        res.send(result);
-    })
-});
-
-router.get('/:gameId/help', function (req, res) {
-    let game = getGame(req.params["gameId"], req);
-    glob(getAbsoluteMediaPath(game) + "/GameHelp/" + game.name + "*.{png,jpg}", function (err, files) {
-        let result = [];
-        for (file of files) {
-            result.push(getRelativeMediaPath(game) + "/GameHelp/" + path.basename(file));
-        }
-        res.send(result);
-    })
-});
-
-router.get('/:gameId/playfield', function (req, res) {
-    let game = getGame(req.params["gameId"], req);
-    glob(getAbsoluteMediaPath(game) + "/Playfield/" + game.name + "*.{png,jpg,mp4}", function (err, files) {
-        let result = [];
-        for (file of files) {
-            result.push(getRelativeMediaPath(game) + "/Playfield/" + path.basename(file));
-        }
-        res.send(result);
-    })
-});
-
-router.get('/:gameId/launch', function (req, res) {
-    let gameId = req.params["gameId"];
-        axios.get(launchUrl + gameId).then((response) => {
-            if (response.status != 200) {
-                res.status(500);
-                res.send('ERROR');
-            } else {
-                res.send('OK');
-            }
-        }).catch(() => {
-            res.status(500);
-            res.send('ERROR');
-        })
-});
-
-router.get('/exit', function (req, res) {
-    axios.get(exitUrl).then((response) => {
-        if (response.status != 200) {
-            res.status(500);
-            res.send('ERROR');
-        } else {
-            res.send('OK');
-        }
-    }).catch(() => {
-        res.status(500);
-        res.send('ERROR');
-    })
-});
-
-router.get('/:gameId', function (req, res) {
-    let gameId = req.params["gameId"];
-
-    if (gameId == "last") {
-        let game = getLastPlayed(req);
-        if (game) {
-            renderGame(req, res, game.id);
-        } else {
-            res.status(404);
-            res.send('NOT FOUND');
-        }
-    } else if (gameId == "current") {
-        axios.get(getCurItemUrl).then((response) => {
-            if (response.status != 200) {
-                res.status(500);
-                res.send('ERROR');
-            } else {
-                gameId = response.data.GameID;
-                renderGame(req, res, gameId);
-            }
-        }).catch(() => {
-            res.status(500);
-            res.send('ERROR');
-        });
-    } else {
-        renderGame(req, res, gameId);
+router.get("/:gameId/info", function (req, res) {
+  let game = getGame(req.params["gameId"], req);
+  glob(
+    getAbsoluteMediaPath(game) + "/GameInfo/" + game.name + "*.{png,jpg}",
+    function (err, files) {
+      let result = [];
+      for (file of files) {
+        result.push(
+          getRelativeMediaPath(game) + "/GameInfo/" + path.basename(file)
+        );
+      }
+      res.send(result);
     }
+  );
+});
+
+router.get("/:gameId/help", function (req, res) {
+  let game = getGame(req.params["gameId"], req);
+  glob(
+    getAbsoluteMediaPath(game) + "/GameHelp/" + game.name + "*.{png,jpg}",
+    function (err, files) {
+      let result = [];
+      for (file of files) {
+        result.push(
+          getRelativeMediaPath(game) + "/GameHelp/" + path.basename(file)
+        );
+      }
+      res.send(result);
+    }
+  );
+});
+
+router.get("/:gameId/playfield", function (req, res) {
+  let game = getGame(req.params["gameId"], req);
+  glob(
+    getAbsoluteMediaPath(game) + "/Playfield/" + game.name + "*.{png,jpg,mp4}",
+    function (err, files) {
+      let result = [];
+      for (file of files) {
+        result.push(
+          getRelativeMediaPath(game) + "/Playfield/" + path.basename(file)
+        );
+      }
+      res.send(result);
+    }
+  );
+});
+
+router.get("/:gameId/launch", function (req, res) {
+  let gameId = req.params["gameId"];
+  axios
+    .get(launchUrl + gameId)
+    .then((response) => {
+      if (response.status != 200) {
+        res.status(500);
+        res.send("ERROR");
+      } else {
+        res.send("OK");
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("ERROR");
+    });
+});
+
+router.get("/exit", function (req, res) {
+  axios
+    .get(exitUrl)
+    .then((response) => {
+      if (response.status != 200) {
+        res.status(500);
+        res.send("ERROR");
+      } else {
+        res.send("OK");
+      }
+    })
+    .catch(() => {
+      res.status(500);
+      res.send("ERROR");
+    });
+});
+
+router.get("/:gameId", function (req, res) {
+  let gameId = req.params["gameId"];
+
+  if (gameId == "last") {
+    let game = getLastPlayed(req);
+    if (game) {
+      renderGame(req, res, game.id);
+    } else {
+      res.status(404);
+      res.send("NOT FOUND");
+    }
+  } else if (gameId == "current") {
+    axios
+      .get(getCurItemUrl)
+      .then((response) => {
+        if (response.status != 200) {
+          res.status(500);
+          res.send("ERROR");
+        } else {
+          gameId = response.data.GameID;
+          renderGame(req, res, gameId);
+        }
+      })
+      .catch(() => {
+        res.status(500);
+        res.send("ERROR");
+      });
+  } else {
+    renderGame(req, res, gameId);
+  }
 });
 
 function getLastPlayed(req) {
-    let sql = 'SELECT g.GameID, LastPlayed, NumberPlays, TimePlayedSecs '
-        + 'FROM Games g JOIN GamesStats s on g.GameID = s.GameID '
-        + 'ORDER BY LastPlayed DESC LIMIT 1';
+  let sql =
+    "SELECT g.GameID, LastPlayed, NumberPlays, TimePlayedSecs " +
+    "FROM Games g JOIN GamesStats s on g.GameID = s.GameID " +
+    "ORDER BY LastPlayed DESC LIMIT 1";
 
-    const db = require('better-sqlite3')(settings.pupServer.db.path, { fileMustExist: true });
-    const row = db.prepare(sql).get();
-    db.close();
+  const db = require("better-sqlite3")(settings.pupServer.db.path, {
+    fileMustExist: true,
+    verbose: debug("app:sql"),
+  });
+  const row = db.prepare(sql).get();
+  db.close();
 
-    if (row) {
-        let game = getGame(row.GameID, req);
-        if (game) {
-            // update with latest stats
-            game.lastPlayed = row.LastPlayed;
-            game.numPlays = row.NumberPlays;
-            game.timePlayed = row.TimePlayedSecs;
-        }
-        return game;
+  if (row) {
+    let game = getGame(row.GameID, req);
+    if (game) {
+      // update with latest stats
+      game.lastPlayed = row.LastPlayed;
+      game.numPlays = row.NumberPlays;
+      game.timePlayed = row.TimePlayedSecs;
     }
-    return;
+    return game;
+  }
+  return;
 }
 
 function getAbsoluteMediaPath(game) {
-    return game.emulator.dirMedia;
+  return game.emulator.dirMedia;
 }
 
 function getRelativeMediaPath(game) {
-    return '/media' + game.emulator.id;
+  return "/media" + game.emulator.id;
 }
 
 function getGame(gameId, req) {
-    let gamePos = req.app.locals.gameIds.get(parseInt(gameId));
-    return gamePos === undefined ? gamePos : req.app.locals.games[gamePos];
+  let gamePos = req.app.locals.gameIds.get(parseInt(gameId));
+  return gamePos === undefined ? gamePos : req.app.locals.games[gamePos];
 }
 
 function renderGame(req, res, gameId) {
-    let game = getGame(gameId, req);
-    if (game) {
-        res.render('game', {
-            game: game,
-            info: settings.options.game.info,
-            help: settings.options.game.help,
-            playfield: settings.options.game.playfield,
-            wheelRotation: settings.media.useThumbs ? req.app.locals.globalSettings.thumbRotation : 0,
-            playfieldRotation: settings.media.playfieldRotation,
-            refreshInterval: req.app.locals.globalSettings.currentGameRefreshTimer
-        });
-    } else {
-        res.status(404);
-        res.send('NOT FOUND');
-    }
+  let game = getGame(gameId, req);
+  if (game) {
+    res.render("game", {
+      game: game,
+      info: settings.options.game.info,
+      help: settings.options.game.help,
+      playfield: settings.options.game.playfield,
+      wheelRotation: settings.media.useThumbs
+        ? req.app.locals.globalSettings.thumbRotation
+        : 0,
+      playfieldRotation: settings.media.playfieldRotation,
+      refreshInterval: req.app.locals.globalSettings.currentGameRefreshTimer,
+    });
+  } else {
+    res.status(404);
+    res.send("NOT FOUND");
+  }
 }
 
 module.exports = router;
